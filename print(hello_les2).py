8e3996d3cd67e523272df785c9362f1dd85a6c63
Mona Lisa <sania5352@gmail.com>
2024-03-10 17:44:43 +0200

    моом

--- print(hello_les2).py
+++ print(hello_les2).py
@@ -1,16 +1,16 @@
 import random
+
 min = 1 
 max = 1000
-quantity = 5
+quantity = int(input("Введіть число : "))
 
 #def get_numbers_ticket(min, max, quantity):
 
-numbers = random.randint(min, max) 
-my_list = [numbers]
-#if quantity > 1:
-   # my_list = my_list.append(numbers)
-print(numbers)
-print(my_list)
+for i in range(quantity):
+    numbers = random.randint(min, max)
+    print(numbers)
+
+list = []
 
 
 
@@ -18,8 +18,10 @@ print(my_list)
 
 
 
-#if quantity > 1:
-#    my_list = numbers +=1
+
+
+
+
 
 
 
