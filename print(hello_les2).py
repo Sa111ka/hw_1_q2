a166157e2da2367da6a4a2f0daf95ffbb92bc0c2
Mona Lisa <sania5352@gmail.com>
2024-03-10 13:49:17 +0200

    ломаю_голову

--- print(hello_les2).py
+++ print(hello_les2).py
@@ -1,1 +1,32 @@
-print("hello_les_2")
+import random
+min = 1 
+max = 1000
+quantity = 5
+
+#def get_numbers_ticket(min, max, quantity):
+
+numbers = random.randint(min, max) 
+my_list = [numbers]
+#if quantity > 1:
+   # my_list = my_list.append(numbers)
+print(numbers)
+print(my_list)
+
+
+
+
+
+
+
+#if quantity > 1:
+#    my_list = numbers +=1
+
+
+
+
+
+
+
+
+
+
