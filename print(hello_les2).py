40d7b684a262936445f6168fc95fa01b4ba907fb
Mona Lisa <sania5352@gmail.com>
2024-03-11 19:49:45 +0200

    my_HW_2

--- print(hello_les2).py
+++ print(hello_les2).py
@@ -1,4 +1,5 @@
 import random
+
 min = 1
 max = 1000
 quantity = int(input("Введіть число : "))
@@ -6,13 +7,14 @@ quantity = int(input("Введіть число : "))
 def get_numbers_ticket(min, max, quantity):
     if min > quantity or max < quantity:
         return []
+    numbers = []
+    for i in range(quantity):
+        number = random.randint(min, max)
+        numbers.append(number)
+    return numbers
 
-
-numbers = random.randint(min, max) 
-for i in range(quantity):
-    numbers = random.randint(min, max)
-    print(numbers)
-
+result = get_numbers_ticket(min, max, quantity)
+print(result)
 
 
 
